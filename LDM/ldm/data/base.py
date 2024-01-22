4353057a50b2454a6293b5c8360a09b50e6df8f2
Claudia <123629072+ClaudiaGiardina90@users.noreply.github.com>
2024-01-22 15:29:35 +0100

    Create base.py

---
+++ LDM/ldm/data/base.py
@@ -0,0 +1,27 @@
+"""
+same code as the one in the original repository
+https://github.com/CompVis/latent-diffusion/blob/main/ldm/data/base.py
+"""
+from abc import abstractmethod
+from torch.utils.data import Dataset, ConcatDataset, ChainDataset, IterableDataset
+
+
+class Txt2ImgIterableBaseDataset(IterableDataset):
+    '''
+    Define an interface to make the IterableDatasets for text2img data chainable
+    '''
+    def __init__(self, num_records=0, valid_ids=None, size=256):
+        super().__init__()
+        self.num_records = num_records
+        self.valid_ids = valid_ids
+        self.sample_ids = valid_ids
+        self.size = size
+
+        print(f'{self.__class__.__name__} dataset contains {self.__len__()} examples.')
+
+    def __len__(self):
+        return self.num_records
+
+    @abstractmethod
+    def __iter__(self):
+        pass
